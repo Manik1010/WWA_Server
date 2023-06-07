103fa6bf40aaaa32a801f725245ab8469f7508d3
Manik Molla <mdblmanikislam777p@gmail.com>
2023-06-08 00:02:14 +0600

    maked instrocture a user

--- index.js
+++ index.js
@@ -60,11 +60,26 @@ async function run() {
     // Admin Panel API................................. 
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+      // console.log(id);
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
           role: 'Admin'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'Instructor'
         },
       };
 
